22ab024b77cdeb25fa05a76e4474d072c51a8804
Thorbjørn Ravn Andersen <thorbjoern@gmail.com>
2012-01-13 13:35:55 +0100

    Maven layout

---
+++ src/main/java/com/norvig/jscheme1998/App.java
@@ -0,0 +1,13 @@
+package com.norvig.jscheme1998;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
